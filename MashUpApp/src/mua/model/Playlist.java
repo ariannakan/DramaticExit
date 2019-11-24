4e004eb3510c27b0b30424f31ac6a120f714753e
kanar <kanar@AriannaAsus>
2019-11-24 15:22:21 -0500

    updated Playlist entity, completed PlaylistsDAO

--- MashUpApp/src/mua/model/Playlist.java
+++ MashUpApp/src/mua/model/Playlist.java
@@ -1,44 +1,40 @@
 package mua.model;
 
+import java.util.*;
+
 public class Playlist {
-	public final String id;
-	public final String name;
+	public final String playlistID;
+	public final String playlistName;
 	public boolean system;      // when TRUE this is actually stored in S3 bucket
 	public ArrayList<String> videos = new ArrayList<String>(); 
-    Iterator iterator = videos.iterator(); //iterator to traverse the list 
+    Iterator<String> iterator = videos.iterator(); //iterator to traverse the list 
+	public String videoID;
 	
 	public Playlist (String id, String name) {
-		this.id = id;
-		this.name = name;
+		this.playlistID = id;
+		this.playlistName = name;
 	}
 	
 	public Playlist (String id, String name, boolean system) {
-		this.id = id;
-		this.name = name;
-		this.sentence = sentence;
+		this.playlistID = id;
+		this.playlistName = name;
 		this.system = system;
 	}
 	
 	public boolean getSystem() { return system; }
 	public void setSystem(boolean s) { system = s; }
 	
-	public boolean addVideo(){}
-	
-	public boolean remVideo(){}
-	
-	//public void showVideo() {}
-	/**
-	 * Equality of Constants determined by name alone.
-	 */
-	public boolean equals (Object o) {
-		if (o == null) { return false; }
-		
-		if (o instanceof Video) {
-			Video other = (Video) o;
-			return id.equals(other.id);
-		}
-		
-		return false;  // not a Constant
+	public boolean addVideo(){
+		return false;
 	}
+	
+	public boolean removeVideo(){
+		return false;
+	}
+	
+	public void showVideo() {}
+	
+	public void listVideos() {}
+	
 }
 
