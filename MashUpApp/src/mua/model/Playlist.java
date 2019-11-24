91052d0135684dbf681e0340bc8741aa26dc9299
Pom <Pom@MSI>
2019-11-24 15:25:24 -0500

    update playlist and library classes

--- MashUpApp/src/mua/model/Playlist.java
+++ MashUpApp/src/mua/model/Playlist.java
@@ -1,44 +1,43 @@
 package mua.model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Playlist {
-	public final String id;
+	public final String playlistID;
 	public final String name;
 	public boolean system;      // when TRUE this is actually stored in S3 bucket
-	public ArrayList<String> videos = new ArrayList<String>(); 
-    Iterator iterator = videos.iterator(); //iterator to traverse the list 
+	public ArrayList<Video> videos = new ArrayList<Video>(); 
+	Iterator<Video> iterator = videos.iterator(); //iterator to traverse the list 
 	
 	public Playlist (String id, String name) {
-		this.id = id;
+		this.playlistID = id;
 		this.name = name;
 	}
 	
 	public Playlist (String id, String name, boolean system) {
-		this.id = id;
+		this.playlistID = id;
 		this.name = name;
-		this.sentence = sentence;
 		this.system = system;
 	}
 	
 	public boolean getSystem() { return system; }
 	public void setSystem(boolean s) { system = s; }
 	
-	public boolean addVideo(){}
+	public boolean addVideo(Video video){
+		videos.add(video);
+	}
 	
-	public boolean remVideo(){}
+	public boolean remVideo(String searchID){
+		while (iterator.hasNext()) {
+			if (iterator.next().videoID.contains(searchID)) {
+				videos.remove(iterator.next());
+			}
+			
+		}
+	}
 	
 	//public void showVideo() {}
-	/**
-	 * Equality of Constants determined by name alone.
-	 */
-	public boolean equals (Object o) {
-		if (o == null) { return false; }
-		
-		if (o instanceof Video) {
-			Video other = (Video) o;
-			return id.equals(other.id);
-		}
-		
-		return false;  // not a Constant
-	}
+	
 }
 
