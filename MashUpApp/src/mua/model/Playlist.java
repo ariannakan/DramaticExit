46ea9ad33d717ceb7c0a695572a691aa86fddcce
ariannakan <kan.ari123@gmail.com>
2019-11-24 16:21:14 -0500

    fixed Playlist class, created uploadVideoHandler

--- MashUpApp/src/mua/model/Playlist.java
+++ MashUpApp/src/mua/model/Playlist.java
@@ -1,56 +1,34 @@
 package mua.model;
 
-<<<<<<< HEAD
 import java.util.ArrayList;
-import java.util.Iterator;
-=======
 import java.util.*;
->>>>>>> branch 'master' of https://github.com/ksnoddy/DramaticExit
 
 public class Playlist {
 	public final String playlistID;
-<<<<<<< HEAD
-	public final String name;
-=======
 	public final String playlistName;
->>>>>>> branch 'master' of https://github.com/ksnoddy/DramaticExit
 	public boolean system;      // when TRUE this is actually stored in S3 bucket
-<<<<<<< HEAD
 	public ArrayList<Video> videos = new ArrayList<Video>(); 
 	Iterator<Video> iterator = videos.iterator(); //iterator to traverse the list 
-=======
-	public ArrayList<String> videos = new ArrayList<String>(); 
-    Iterator<String> iterator = videos.iterator(); //iterator to traverse the list 
 	public String videoID;
->>>>>>> branch 'master' of https://github.com/ksnoddy/DramaticExit
 	
 	public Playlist (String id, String name) {
 		this.playlistID = id;
-<<<<<<< HEAD
-		this.name = name;
-=======
 		this.playlistName = name;
->>>>>>> branch 'master' of https://github.com/ksnoddy/DramaticExit
 	}
 	
 	public Playlist (String id, String name, boolean system) {
 		this.playlistID = id;
-<<<<<<< HEAD
-		this.name = name;
-=======
 		this.playlistName = name;
->>>>>>> branch 'master' of https://github.com/ksnoddy/DramaticExit
 		this.system = system;
 	}
 	
 	public boolean getSystem() { return system; }
 	public void setSystem(boolean s) { system = s; }
 	
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
 	public boolean addVideo(Video video){
 		videos.add(video);
+		return true;
 	}
 	
 	public boolean remVideo(String searchID){
@@ -58,25 +36,9 @@ public class Playlist {
 			if (iterator.next().videoID.contains(searchID)) {
 				videos.remove(iterator.next());
 			}
-			
 		}
+		return true;
 	}
 	
-	//public void showVideo() {}
-=======
-	public boolean addVideo(){
-		return false;
-	}
-	
-	public boolean removeVideo(){
-		return false;
-	}
-	
-	public void showVideo() {}
-	
-	public void listVideos() {}
->>>>>>> branch 'master' of https://github.com/ksnoddy/DramaticExit
-	
->>>>>>> branch 'master' of https://github.com/ksnoddy/DramaticExit.git
 }
 
