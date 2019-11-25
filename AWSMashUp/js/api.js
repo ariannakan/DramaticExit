322cf6e7211420a3a1e9f27c7fa2f4d113dd00fb
ariannakan <kan.ari123@gmail.com>
2019-11-25 00:47:50 -0500

    created js files for delete_video, get_videoList, and api.js

--- AWSMashUp/js/api.js
+++ AWSMashUp/js/api.js
@@ -8,8 +8,17 @@ var delete_url = base_url + "delete";       // Can't send JSON to DELETE request
 var list_url   = base_url + "constants";    // GET
 **/
 
-var add_playlist = base_url + "playlist";	//GET
-var add_site	 = base_url + "site";		//GET + POST
-var delete_site	 = base_url + "site";		//DELETE
-var add_video 	 = base_url + "video";		//POST
-var get_video	 = base_url + "video";		//GET
+var list_playlists	= base_url + "library/playlist";	//GET
+var add_site	 	= base_url + "library/site";		//POST
+var get_site	 	= base_url + "library/site";		//GET
+var delete_site	 	= base_url + "library/site/{url}";	//DELETE
+var add_video 	 	= base_url + "library/video";		//POST
+var get_video	 	= base_url + "library/video";		//GET
+var search_video	= base_url + "library/video/search"	//POST
+var delete_video 	= base_url + "library/video/{videoID}"	//DELETE
+var update_video	= base_url + "library/video/{videoID}"	//PUT
+var create_playlist	= base_url + "playlist"				//POST
+var get_playlist	= base_url + "playlist/{playlistID}"	//GET
+var append_video	= base_url + "playlist/{playlistID}"	//POST
+var delete_playlist	= base_url + "playlist/{playlistID}"	//DELETE
+var delete_playlist_video	= base_url + "playlist/{playlistID}/video/{videoID}"	//DELETE
---
+++ AWSMashUp/js/get_videoList.js
@@ -0,0 +1,55 @@
+/**
+ * Refresh constant list from server
+ *
+ *    GET list_url
+ *    RESPONSE  list of [name, value] constants 
+ */
+function refreshVideoList() {
+   var xhr = new XMLHttpRequest();
+   xhr.open("GET", list_url, true);
+   xhr.send();
+   
+   console.log("sent");
+
+  // This will process results and update HTML as appropriate. 
+  xhr.onloadend = function () {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      console.log ("XHR:" + xhr.responseText);
+      processListResponse(xhr.responseText);
+    } else {
+      processListResponse("N/A");
+    }
+  };
+}
+
+/**
+ * Respond to server JSON object.
+ *
+ * Replace the contents of 'constantList' with a <br>-separated list of name,value pairs.
+ */
+function processListResponse(result) {
+  console.log("res:" + result);
+  // Can grab any DIV or SPAN HTML element and can then manipulate its contents dynamically via javascript
+  var js = JSON.parse(result);
+  var vidList = document.getElementById('videoList');
+  
+  var output = "";
+  for (var i = 0; i < js.list.length; i++) {
+    var videoJson = js.list[i];
+    console.log(videoJson);
+    
+    var vname = videoJson["name"];
+    var character = videoJson["character"];
+    var sentence = videoJson["sentence"];
+    var sysvar = videoJson["system"];
+    if (sysvar) {
+    	output = output + "<div id=\"video" + vname + "\"><b>" + vname + ":</b> = " + character + "\"><b>" + vname + ":</b> = " + sentence + "<br></div>";
+    } else {
+    	output = output + "<div id=\"video" + vname + "\"><b>" + vname + ":</b> = " + character + "\"><b>" + vname + ":</b> = " + sentence + "(<a href='javaScript:requestVidDelete(\"" + vname + "\")'><img src='deleteIcon.png'></img></a>) <br></div>";
+    }
+  }
+
+  // Update computation result
+  vidList.innerHTML = output;
+}
+
