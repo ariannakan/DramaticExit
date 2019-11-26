b2dfddc8aac435f98c04071cc2f34a2f8b5c14b2
Michelle <Michelle@130.215.217.74>
2019-11-26 18:18:19 -0500

    updated get playlist json file

--- AWSMashUp/js/get_playlistList.js
+++ AWSMashUp/js/get_playlistList.js
@@ -4,7 +4,7 @@
  *    GET list_url
  *    RESPONSE  list of [name, value] constants 
  */
-function refreshVideoList() {
+function refreshPlaylistList() {
    var xhr = new XMLHttpRequest();
    xhr.open("GET", list_playlists_url, true);
    xhr.send();
@@ -14,7 +14,7 @@ function refreshVideoList() {
   // This will process results and update HTML as appropriate. 
   xhr.onloadend = function () {
     if (xhr.readyState == XMLHttpRequest.DONE) {
-      console.log ("XHR:" + xhr.responseText);
+      //console.log ("XHR:" + xhr.responseText);
       processListResponse(xhr.responseText);
     } else {
       processListResponse("N/A");
@@ -28,28 +28,21 @@ function refreshVideoList() {
  * Replace the contents of 'videoList' with a <br>-separated list of name,value pairs.
  */
 function processListResponse(result) {
-  console.log("res:" + result);
+  console.log("res:" + JSON.parse(result).list);
   // Can grab any DIV or SPAN HTML element and can then manipulate its contents dynamically via javascript
   var js = JSON.parse(result);
-  var vidList = document.getElementById('playlistList');
+  var plList = document.getElementById('playlistList');
+  var list = JSON.parse(result).list
   
-  var output = "";
+  for (var i = 0; i < list.length; i++){
+	  playlist = "<div id=\"" + list[I].playlistID + "\"><b>" + list[I].playlistName + ":</b><br></div>";
+      document.getElementById("playlistList").innerHTML += playlist; 
+  }
+  
   for (var i = 0; i < js.list.length; i++) {
-    var videoJson = js.list[i];
-    console.log(videoJson);
-    
-    var videoID = videoJson["videoID"];
-    var character = videoJson["character"];
-    var sentence = videoJson["sentence"];
-    var sysvar = videoJson["system"];
-    if (sysvar) {
-    	output = output + "<div id=\"const" + videoID + "\"><b>" + videoID + ":</b> = " + character + "\"><b>" + videoID + ":</b> = " + sentence + "<br></div>";
-    } else {
-    	output = output + "<div id=\"const" + videoID + "\"><b>" + videoID + ":</b> = " + character + "\"><b>" + videoID + ":</b> = " + sentence + "(<a href='javaScript:requestVidDelete(\"" + vname + "\")'><img src='deleteIcon.png'></img></a>) <br></div>";
-    }
+    var list = js.list[i];
+    console.log(list);
   }
 
-  // Update computation result
-  vidList.innerHTML = output;
 }
 
