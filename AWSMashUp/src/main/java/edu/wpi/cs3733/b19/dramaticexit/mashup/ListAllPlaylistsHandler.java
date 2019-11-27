84d3a5f5fec57f98b241b791f337c645f7e82fb8
ariannakan <kan.ari123@gmail.com>
2019-11-26 19:15:41 -0500

    working listPlaylists handler --> lambda function

--- AWSMashUp/src/main/java/edu/wpi/cs3733/b19/dramaticexit/mashup/ListAllPlaylistsHandler.java
+++ AWSMashUp/src/main/java/edu/wpi/cs3733/b19/dramaticexit/mashup/ListAllPlaylistsHandler.java
@@ -2,24 +2,52 @@ package edu.wpi.cs3733.b19.dramaticexit.mashup;
 
 import java.util.List;
 
+import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import edu.wpi.cs3733.b19.dramaticexit.mashup.db.PlaylistsDAO;
+import edu.wpi.cs3733.b19.dramaticexit.mashup.http.AllPlaylistsResponse;
+import edu.wpi.cs3733.b19.dramaticexit.mashup.http.ListPlaylistsRequest;
 import edu.wpi.cs3733.b19.dramaticexit.mashup.model.Playlist;
+import edu.wpi.cs3733.b19.dramaticexit.mashup.model.Video;
 
-public class ListAllPlaylistsHandler {
+public class ListAllPlaylistsHandler implements RequestHandler<ListPlaylistsRequest,AllPlaylistsResponse>{
 	
-	public LambdaLogger logger;
+	LambdaLogger logger;
 
 	/** Load from RDS, if it exists
 	 * 
 	 * @throws Exception 
 	 */
 	List<Playlist> getPlaylists() throws Exception {
-		logger.log("in getVideos");
 		PlaylistsDAO dao = new PlaylistsDAO();
 		
 		return dao.getAllPlaylists();
+	}
+
+	@Override
+	public AllPlaylistsResponse handleRequest(ListPlaylistsRequest input, Context context) {
+		logger = context.getLogger();
+		logger.log("Loading Java Lambda handler to list all videos");
+
+		AllPlaylistsResponse response;
+		try {
+			// get all user defined constants AND system-defined constants.
+			// Note that user defined constants override system-defined constants.
+			List<Playlist> list = getPlaylists();
+		
+//			for (Video v : systemVideos()) {
+//				if (!list.contains(v)) {
+//					list.add(v);
+//				}
+//			}
+			response = new AllPlaylistsResponse(list, 200);
+		} catch (Exception e) {
+			response = new AllPlaylistsResponse(403, e.getMessage());
+		}
+		
+		return response;
 	}
 
 }
