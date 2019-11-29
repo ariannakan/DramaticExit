7bb42e7281c88ba6f6cc51caacaa9d677beb9dc0
Ken Snoddy <ksnoddy@wpi.edu>
2019-11-29 16:19:53 -0500

    Black Friday HTML Updates

--- AWSMashUp/js/get_videoList.js
+++ AWSMashUp/js/get_videoList.js
@@ -38,7 +38,7 @@ function processListResponse(result) {
   for (var I = 0; I < list.length; I++)
   {
        video = "<div><b>Video Segment " + I + "</b></div>" + "<div><video id=\""+ list[I].videoID + "\"height=\"" +150+ "\" controls>" + "<source src=\""+list[I].url+ "\" type=\"video/ogg\"></video>" + "</></div>" +
-       		"<div><b>" + list[I].characterName + ":</b>" + list[I].sentence + "</></div>"
+       		"<div><b>" + list[I].characterName + ": </b>" + list[I].sentence + "</></div>"
        document.getElementById("videoList").innerHTML += video;
   }
   
