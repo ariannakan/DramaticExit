21a6f82b3e71a6fdaebedc7e3fa09276fd5b6e8a
Pom <Pom@130.215.125.218>
2019-12-02 19:50:19 -0500

    Edited uploadvideohandler(not complete)

--- AWSMashUp/src/main/java/edu/wpi/cs3733/b19/dramaticexit/mashup/UploadVideoHandler.java
+++ AWSMashUp/src/main/java/edu/wpi/cs3733/b19/dramaticexit/mashup/UploadVideoHandler.java
@@ -1,6 +1,8 @@
 package edu.wpi.cs3733.b19.dramaticexit.mashup;
 
 import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.Context;
@@ -13,10 +15,6 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
 
-import edu.wpi.cs.heineman.calculator.db.ConstantsDAO;
-import edu.wpi.cs.heineman.calculator.http.CreateConstantRequest;
-import edu.wpi.cs.heineman.calculator.http.CreateConstantResponse;
-import edu.wpi.cs.heineman.calculator.model.Constant;
 import edu.wpi.cs3733.b19.dramaticexit.mashup.db.VideosDAO;
 import edu.wpi.cs3733.b19.dramaticexit.mashup.http.UploadVideoRequest;
 import edu.wpi.cs3733.b19.dramaticexit.mashup.http.UploadVideoResponse;
@@ -28,6 +26,30 @@ LambdaLogger logger;
 	
 	// To access S3 storage
 	private AmazonS3 s3 = null;
+	
+	/** Create S3 bucket
+	 * 
+	 * @throws Exception 
+	 */
+	boolean uploadVideo(String oggFile) throws Exception {
+		if (logger != null) { logger.log("in uploadVideo"); }
+		
+		if (s3 == null) {
+			logger.log("attach to S3 request");
+			s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_2).build();
+			logger.log("attach to S3 succeed");
+		}
+		
+		InputStream inputstream = new FileInputStream(oggFile);
+		ObjectMetadata omd = new ObjectMetadata();
+		omd.setContentLength(oggFile.length());
+		
+		PutObjectResult res = s3.putObject(new PutObjectRequest("b19dramaticexit", "Videos/" + name, bais, omd)
+				.withCannedAcl(CannedAccessControlList.PublicRead));
+		
+		// if we ever get here, then whole thing was stored
+		return true;
+	}
 		
 	/** Store into RDS.
 	 * 
@@ -38,39 +60,13 @@ LambdaLogger logger;
 		VideosDAO dao = new VideosDAO();
 		
 		// check if present
-		Video exist = dao.getVideo(videoID);
+		Video exist = dao.getVideo(sentence);
 		Video video = new Video (videoID, characterName, sentence, availability, url);
 		if (exist == null) {
 			return dao.addVideo(video);
 		} else {
 			return false;
 		}
-	}
-	
-	/** Create S3 bucket
-	 * 
-	 * @throws Exception 
-	 */
-	boolean createVideo(String oggFile) throws Exception {
-		if (logger != null) { logger.log("in createVideo"); }
-		
-		if (s3 == null) {
-			logger.log("attach to S3 request");
-			s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_2).build();
-			logger.log("attach to S3 succeed");
-		}
-		
-		ByteArrayInputStream bais = new ByteArrayInputStream(oggFile);
-		ObjectMetadata omd = new ObjectMetadata();
-		omd.setContentLength(oggFile.length());
-		
-		PutObjectResult res = s3.putObject(new PutObjectRequest("b19dramaticexit", "Videos/" + name, bais, omd));
-		// makes the object publicly visible
-		PutObjectResult res = s3.putObject(new PutObjectRequest("cs3733wpi", "constants/" + name, bais, omd)
-				.withCannedAcl(CannedAccessControlList.PublicRead));
-		
-		// if we ever get here, then whole thing was stored
-		return true;
 	}
 	
 	@Override 
@@ -82,23 +78,23 @@ LambdaLogger logger;
 		try {
 			byte[] encoded = java.util.Base64.getDecoder().decode(req.base64EncodedValue);
 			if (req.system) {
-				if (createSystemConstant(req.name, encoded)) {
-					response = new CreateConstantResponse(req.name);
+				if (uploadVideotoRDS(req.videoID, encoded)) {
+					response = new UploadVideoResponse(req.videoID);
 				} else {
-					response = new CreateConstantResponse(req.name, 422);
+					response = new UploadVideoResponse(req.videoID, 422);
 				}
 			} else {
 				String contents = new String(encoded);
 				double value = Double.valueOf(contents);
 				
-				if (createConstant(req.name, value)) {
-					response = new CreateConstantResponse(req.name);
+				if (uploadVideotoRDS(req.videoID, value)) {
+					response = new UploadVideoResponse(req.videoID);
 				} else {
-					response = new CreateConstantResponse(req.name, 422);
+					response = new UploadVideoResponse(req.videoID, 422);
 				}
 			}
 		} catch (Exception e) {
-			response = new CreateConstantResponse("Unable to create constant: " + req.name + "(" + e.getMessage() + ")", 400);
+			response = new UploadVideoResponse("Unable to create constant: " + req.videoID + "(" + e.getMessage() + ")", 400);
 		}
 
 		return response;
