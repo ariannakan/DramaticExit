f2b28d0bf47626580176e25a5276df276056c9bd
Michelle <Michelle@LAPTOP-2SE7UF4E>
2019-12-02 22:43:37 -0500

    edited delete button

--- AWSMashUp/js/get_videoList.js
+++ AWSMashUp/js/get_videoList.js
@@ -36,8 +36,8 @@ function processListResponse(result) {
   
   for (var I = 0; I < list.length; I++) {
 	  video = "<div id=\"" + list[I].videoID + "\">" +
-	  		"<div><b><center>" + list[I].videoID + "</center></b>" +
-	  		"<div><center><a href='javaScript:requestVidDelete(\"" + list[I].videoID + "\")'><img src='https://cdn0.iconfinder.com/data/icons/very-basic-android-l-lollipop-icon-pack/24/close-128.png' height="+ 20 +" width="+ 20 +"></img></a></center>" +
+	  		"<div><b><center>" + list[I].videoID + "</b>     " +
+	  		"<a href='javaScript:requestVidDelete(\"" + list[I].videoID + "\")'><img src='trashcan.png' height=" + 14 + "></img></a></center>" +
        		"<div><video height=" + 150 + " controls>" + "<source src=\"" + list[I].url + "\" type=\"video/ogg\"></video>" +
        		"<div><b>" + list[I].characterName + ": </b>" + list[I].sentence + "</></div>";
 	  document.getElementById("videoList").innerHTML += video;
