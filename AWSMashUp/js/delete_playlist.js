7b94c80655a14b27c58be7d5c50f6eeb2b9424a1
Ken Snoddy <ksnoddy@wpi.edu>
2019-12-04 18:53:23 -0500

    Added .ogg file 2

---
+++ AWSMashUp/js/delete_playlist.js
@@ -0,0 +1,48 @@
+function processDeleteResponse(result) {
+  // Can grab any DIV or SPAN HTML element and can then manipulate its
+  // contents dynamically via javascript
+  console.log("deleted:" + result);
+  
+  refreshPlaylistList();
+  //might need to include refresh playlistlist
+  //refreshRemoteList();
+}
+
+function requestPlaylistDelete(playlist) {
+   if (confirm("Request to delete " + playlist)) {
+     processDelete(playlist);
+   }
+}
+
+function processDelete(playlist) {
+  var data = {};
+  data["playlistID"] = playlist;
+
+  var js = JSON.stringify(data);
+  console.log("JS:" + js);
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", delete_playlist_url, true);  // Can't be DELETE since then no data sent via JSON
+
+  // send the collected data as JSON
+  xhr.send(js);
+
+  // This will process results and update HTML as appropriate. 
+  xhr.onloadend = function () {
+	  console.log(xhr);
+	  console.log(xhr.request);
+	  if (xhr.readyState == XMLHttpRequest.DONE) {
+		  if (xhr.status == 200) {
+			  console.log ("XHR:" + xhr.responseText);
+			  processDeleteResponse(xhr.responseText);
+		  } else {
+			  console.log("actual:" + xhr.responseText)
+			  var js = JSON.parse(xhr.responseText);
+			  var err = js["error"];
+			  alert (err);
+		  }
+	  } else {
+		  processDeleteResponse("N/A");
+	  }
+  };
+}
+
