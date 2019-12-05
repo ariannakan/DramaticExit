980aef9f07cf9bbb132b9cc878d2dec3e7bb9e64
pphongsopa <pphongsopa@wpi.edu>
2019-12-05 18:46:32 -0500

    update_video js

--- AWSMashUp/js/update_video.js
+++ AWSMashUp/js/update_video.js
@@ -1,3 +1,54 @@
 /**
  * 
  */
+ 
+ /**
+  * 
+  */
+ function processUpdateVideoResponse(result) {
+   // Can grab any DIV or SPAN HTML element and can then manipulate its
+   // contents dynamically via javascript
+   console.log("Update Availability:" + result);
+   
+   refreshVideoList();
+ }
+
+ function processUpdateVideoRequest(vid, mark) {
+    if (confirm("Request to delete " + vid)) {
+      processUpdateVideo(vid, mark);
+    }
+ }
+
+ function processUpdateVideo(vid, mark) {
+   var data = {};
+   data["videoID"] = vid; //form????
+   data["availability"] = mark;//form??
+   
+   var js = JSON.stringify(data);
+   console.log("JS:" + js);
+   var xhr = new XMLHttpRequest();
+   xhr.open("PUT", update_video_url, true);  // Can't be DELETE since then no data sent via JSON
+
+   // send the collected data as JSON
+   xhr.send(js);
+
+   // This will process results and update HTML as appropriate. 
+   xhr.onloadend = function () {
+ 	  console.log(xhr);
+ 	  console.log(xhr.request);
+ 	  if (xhr.readyState == XMLHttpRequest.DONE) {
+ 		  if (xhr.status == 200) {
+ 			  console.log ("XHR:" + xhr.responseText);
+ 			  processDeleteResponse(xhr.responseText);
+ 		  } else {
+ 			  console.log("actual:" + xhr.responseText)
+ 			  var js = JSON.parse(xhr.responseText);
+ 			  var err = js["error"];
+ 			  alert (err);
+ 		  }
+ 	  } else {
+ 		  processDeleteResponse("N/A");
+ 	  }
+   };
+ }
+
