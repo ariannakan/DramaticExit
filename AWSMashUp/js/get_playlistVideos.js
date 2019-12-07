7f73fb2d1f52140f5c577c7963dad63f910ca521
pphongsopa <pphongsopa@wpi.edu>
2019-12-07 16:56:08 -0500

    empty playlist

--- AWSMashUp/js/get_playlistVideos.js
+++ AWSMashUp/js/get_playlistVideos.js
@@ -61,47 +61,28 @@ function processPlaylistVideosResponse(result) {
   var playlistVideosList = document.getElementById('playlistVideosList');
   
   var output = "";
-  for (var i = 0; i < js.list.length; i++) {
-    var videoJson = js.list[i];
-    console.log(videoJson);
-    
-    var videoID = videoJson["videoID"];
-    var url = videoJson["url"];
-    var characterName = videoJson["characterName"];
-    var sentence = videoJson["sentence"];
-    
-    videoSource[i] = url;
-    
-    output = output + "<div id=\"" + videoID + "\">" + 
-		"<center>(<a href='javaScript:requestRemoveVideo(\"" + plName + "\",\"" + videoID + "\")' style = 'filter: invert(22%) sepia(95%) saturate(7454%) hue-rotate(360deg) brightness(100%) contrast(117%)'><img src='trashcan.png' height=" + 14 + "></img></a>)" +
-		"<br><video height=" + 150 + " controls>" + "<source src=\"" + url + "\" type=\"video/ogg\"></video>" + "</><br></center></div>";
-	
-  }
-  
-//  Play videos one after another    
-//  var 2019.12.07.01.04.33 = document.getElementById("2019.12.07.01.04.33");
-//  2019.12.07.01.04.33.addEventListener("ended", function() {2019.12.07.01.05.18.play(); });
-//  var 2019.12.07.01.05.18 = document.getElementById("2019.12.07.01.05.18");
-//  2019.12.07.01.05.18.addEventListener("ended", function() {2019.12.07.01.05.37.play(); });
-//  var 2019.12.07.01.05.37 = document.getElementById("2019.12.07.01.05.37");
-//  2019.12.07.01.05.37.addEventListener("ended", function() {2019.12.07.01.06.03.play(); });
-//  var 2019.12.07.01.06.03 = document.getElementById("2019.12.07.01.06.03");
-//  2019.12.07.01.06.03.addEventListener("ended", function() {2019.12.07.01.06.32.play(); });
-//  var 2019.12.07.01.06.32 = document.getElementById("2019.12.07.01.06.32");
-//  2019.12.07.01.06.32.addEventListener("ended", function() {2019.12.07.01.06.57.play(); });
-//  var 2019.12.07.01.06.57 = document.getElementById("2019.12.07.01.06.57");
-//  2019.12.07.01.06.57.addEventListener("ended", function() {2019.12.07.01.07.16.play(); });
-//  var 2019.12.07.01.07.16 = document.getElementById("2019.12.07.01.07.16");
-//  2019.12.07.01.07.16.addEventListener("ended", function() {2019.12.07.01.07.38.play(); });
-//  var 2019.12.07.01.07.38 = document.getElementById("2019.12.07.01.07.38");
-//  2019.12.07.01.07.38.addEventListener("ended", function() {2019.12.07.01.07.56.play(); });
-//  var 2019.12.07.01.07.56 = document.getElementById("2019.12.07.01.07.56");
-//  2019.12.07.01.07.56.addEventListener("ended", function() {2019.12.07.01.08.11.play(); });
 
-  
-  
+  if (js.list == ""){
+	  console.log("no videos in playlist");
+	  output = output + "<p style='text-indent: 20px'><b>No videos in this playlist</b></p>"
+  } else {
+	  for (var i = 0; i < js.list.length; i++) {
+		var videoJson = js.list[i];
+    	console.log(videoJson);
+    
+    	var videoID = videoJson["videoID"];
+    	var url = videoJson["url"];
+    	var characterName = videoJson["characterName"];
+    	var sentence = videoJson["sentence"];
+    
+    	videoSource[i] = url;
+    
+    	output = output + "<div id=\"" + videoID + "\">" + 
+			"<center>(<a href='javaScript:requestRemoveVideo(\"" + plName + "\",\"" + videoID + "\")' style = 'filter: invert(22%) sepia(95%) saturate(7454%) hue-rotate(360deg) brightness(100%) contrast(117%)'><img src='trashcan.png' height=" + 14 + "></img></a>)" +
+			"<br><video height=" + 150 + " controls>" + "<source src=\"" + url + "\" type=\"video/ogg\"></video>" + "</><br></center></div>";
+  		}
+  	}
   playlistVideosList.innerHTML = output;
-
 }
 
 
