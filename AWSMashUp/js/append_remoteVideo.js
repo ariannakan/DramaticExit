2a3ef0f41882fb37c95f9c0c4090efe440989c0c
pphongsopa <pphongsopa@wpi.edu>
2019-12-11 22:52:40 -0500

    append_remoteVideo js

---
+++ AWSMashUp/js/append_remoteVideo.js
@@ -0,0 +1,93 @@
+function processAppendRemoteVideoResponse(result, playlistName) {
+  // Can grab any DIV or SPAN HTML element and can then manipulate its
+  // contents dynamically via javascript
+  console.log("Append: " + result);
+  
+  //refreshPlaylistList();
+  requestPlaylistVideos(playlistName);
+  //requestDisplayAvailableVideos(playlistName)
+  //might need to include refresh playlistlist
+  //refreshRemoteList();
+}
+
+function requestDisplayAvailableRemoteVideos(playlistName){
+	var xhr = new XMLHttpRequest();
+	   xhr.open("POST", get_remote_videos_url, true);
+	   xhr.send();
+	   
+	   console.log("sent");
+
+	  // This will process results and update HTML as appropriate. 
+	  xhr.onloadend = function () {
+	    if (xhr.readyState == XMLHttpRequest.DONE) {
+	     console.log ("XHR:" + xhr.responseText);
+	     processAppendRemoteListResponse(playlistName, xhr.responseText);
+	    } else {
+	      processRemoteListResponse("N/A");
+	    }
+	  };
+}
+
+function processAppendRemoteListResponse(playlistName, result) {
+	  //console.log("res:" , JSON.parse(result).list);
+	  console.log("append result:" + result);
+	  // Can grab any DIV or SPAN HTML element and can then manipulate its contents dynamically via javascript
+	  var js = JSON.parse(result);
+	  var vidList = document.getElementById('appendRemoteVideoList');
+	  
+	  var output = "";
+	  for (var i = 0; i < js.list.length; i++) {
+	    var videoJson = js.list[i];
+	    console.log(videoJson);
+	    
+	    var availability = videoJson["availability"];
+	    var characterName = videoJson["characterName"];
+	    var sentence = videoJson["sentence"];
+	    var videoID = videoJson["videoID"];
+	    var url = videoJson["url"];
+	    
+//		 output = output + "<div id=\"vid" + videoID + "\">" +
+//	    	"<br><b>" + characterName + ": </b>" + sentence + "</> (<a href='javaScript:processAppendVideoRequest(\"" + playlistName + "\",\"" + videoID + "\")' class = 'viewAppendVideo'>Append</a>)<br></div>";
+
+		 output = output + "<div id=\"vid" + videoID + "\">" +
+	    	"<br>(<a href='javaScript:processAppendRemoteVideoRequest(\"" + playlistName + "\",\"" + videoID + "\")' class = 'viewAppendRemoteVideo'>Append</a>) <b>" + characterName + ": </b>" + sentence + "</><br></div>";	
+	  }
+	  
+	  vidList.innerHTML = output;
+
+	}
+
+
+function processAppendRemoteVideoRequest(playlistName, videoID) {
+  var data = {};
+  data["playlistName"] = playlistName
+  data["videoID"] = videoID;
+
+  var js = JSON.stringify(data);
+  console.log("JS:" + js);
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", append_video_url, true);  // Can't be DELETE since then no data sent via JSON
+
+  // send the collected data as JSON
+  xhr.send(js);
+
+  // This will process results and update HTML as appropriate. 
+  xhr.onloadend = function () {
+	  console.log(xhr);
+	  console.log(xhr.request);
+	  if (xhr.readyState == XMLHttpRequest.DONE) {
+		  if (xhr.status == 200) {
+			  console.log ("XHR:" + xhr.responseText);
+			  processAppendRemoteVideoResponse(xhr.responseText, playlistName);
+		  } else {
+			  console.log("actual:" + xhr.responseText)
+			  var js = JSON.parse(xhr.responseText);
+			  var err = js["error"];
+			  alert (err);
+		  }
+	  } else {
+		  processAppendRemoteVideoResponse("N/A");
+	  }
+  };
+}
+
--- AWSMashUp/js/get_remoteVideoList.js
+++ AWSMashUp/js/get_remoteVideoList.js
@@ -6,7 +6,7 @@
  */
 function refreshRemoteVideoList(user) {
    var xhr = new XMLHttpRequest();
-   xhr.open("GET", get_remote_videos_url, true);
+   xhr.open("POST", get_remote_videos_url, true);
    xhr.send();
    
    console.log("sent");
@@ -15,13 +15,9 @@ function refreshRemoteVideoList(user) {
   xhr.onloadend = function () {
     if (xhr.readyState == XMLHttpRequest.DONE) {
      // console.log ("XHR:" + xhr.responseText);
-      if(user === "admin"){
-    	  processRemoteSegmentListResponse(xhr.responseText);
-      }
-      else if(user === "append"){
+      if(user === "append"){
     	  processAppendRemoteListResponse(xhr.responseText);
-      }
-      else{
+      }else{
     	  processRemoteListResponse(xhr.responseText);
       }
       //processSegmentListResponse(xhr.responseText);
@@ -30,13 +26,13 @@ function refreshRemoteVideoList(user) {
     }
   };
 }
-
+//for playlist.html visibility 
 function processAppendRemoteListResponse(result) {
 	  //console.log("res:" , JSON.parse(result).list);
 	  console.log("append result:" + result);
 	  // Can grab any DIV or SPAN HTML element and can then manipulate its contents dynamically via javascript
 	  var js = JSON.parse(result);
-	  var vidList = document.getElementById('appendVideoList');
+	  var vidList = document.getElementById('appendRemoteVideoList');
 	  
 	  var output = "";
 	  for (var i = 0; i < js.list.length; i++) {
@@ -67,88 +63,33 @@ function processAppendRemoteListResponse(result) {
 	  vidList.innerHTML = output;
 
 	}
-
-function processRemoteSegmentListResponse(result) {
-	  //console.log("res:" , JSON.parse(result).list);
-	  console.log("segment result:" + result);
-	  // Can grab any DIV or SPAN HTML element and can then manipulate its contents dynamically via javascript
-	  var js = JSON.parse(result);
-	  var vidList = document.getElementById('segmentVideoList');
-	  
-	  var output = "";
-	  for (var i = 0; i < js.list.length; i++) {
-	    var videoJson = js.list[i];
-	    console.log(videoJson);
-	    
-	    var availability = videoJson["availability"];
-	    var characterName = videoJson["characterName"];
-	    var sentence = videoJson["sentence"];
-	    var videoID = videoJson["videoID"];
-	    var url = videoJson["url"];
-	    
-	    if(availability === true){
-		    output = output + "<div id=\"vid" + videoID + "\">" +
-				"<br><b><center>Video " + videoID + "</b> " +
-		  		"(<a href='javaScript:requestVidDelete(\"" + videoID + "\")' style = 'filter: invert(22%) sepia(95%) saturate(7454%) hue-rotate(360deg) brightness(100%) contrast(117%)'><img src='trashcan.png' height=" + 14 + "></img></a>)" +
-		   		"<br><video height=" + 150 + " controls>" + "<source src=\"" + url + "\" type=\"video/ogg\"></video>" +
-		   		"<br><b>" + characterName + ": </b>" + sentence + 
-		   		"<br>(<a href='javaScript:processHideVideo(\"" + videoID + "\")' class = 'availButton'>Hide</a>)" + "</><br></center></div>";
-	    } else {
-	    	output = output + "<div id=\"vid" + videoID + "\">" +
-				"<br><b><center>Video " + videoID + "</b> " +
-		  		"(<a href='javaScript:requestVidDelete(\"" + videoID + "\")' style = 'filter: invert(22%) sepia(95%) saturate(7454%) hue-rotate(360deg) brightness(100%) contrast(117%)'><img src='trashcan.png' height=" + 14 + "></img></a>)" +
-		   		"<br><video height=" + 150 + " controls>" + "<source src=\"" + url + "\" type=\"video/ogg\"></video>" +
-		   		"<br><b>" + characterName + ": </b>" + sentence + 
-		   		"<br>(<a href='javaScript:processShowVideo(\"" + videoID + "\")' class = 'availButton'>Show</a>)" + "</><br></center></div>";
-	    }
-	    
-//	    output = output + "<div id=\"" + videoID + "\">" +
-//		"<br><b>" + characterName + ": </b>" + sentence + "</b>" +
-//		"(<a href='javaScript:processShowVideo(\"" + videoID + "\")'>show</a>)</center>" + 
-//		"(<a href='javaScript:processHideVideo(\"" + videoID + "\")'>hide</a>)</center>" + "</><br></div>";
-//			
-	  }
-	  
-	  vidList.innerHTML = output;
-
-	}
-
 /**
  * Respond to server JSON object.
  *
  * Replace the contents of 'videoList' with a <br>-separated list of name,value pairs.
  */
+//for admin and user videos visibility
 function processRemoteListResponse(result) {
   //console.log("res:" , JSON.parse(result).list);
   console.log("res:" + result);
   // Can grab any DIV or SPAN HTML element and can then manipulate its contents dynamically via javascript
   var js = JSON.parse(result);
-  var vidList = document.getElementById('videoList');
+  var vidList = document.getElementById('remoteVideoList');
   
   var output = "";
   for (var i = 0; i < js.list.length; i++) {
     var videoJson = js.list[i];
     console.log(videoJson);
     
-    var availability = videoJson["availability"];
-    var videoID = videoJson["videoID"];
+    var videoID = "Remote";
     var url = videoJson["url"];
     var characterName = videoJson["characterName"];
     var sentence = videoJson["sentence"];
     
-    if(availability === true){
-	    output = output + "<div id=\"vid" + videoID + "\">" +
-			"<br><b><center>Video " + videoID + "</b>     " +
-	  		"(<a href='javaScript:requestVidDelete(\"" + videoID + "\")' style = 'filter: invert(22%) sepia(95%) saturate(7454%) hue-rotate(360deg) brightness(100%) contrast(117%)'><img src='trashcan.png' height=" + 14 + "></img></a>)" +
-	   		"<br><video height=" + 150 + " controls>" + "<source src=\"" + url + "\" type=\"video/ogg\"></video>" +
-	   		"<br><b>" + characterName + ": </b>" + sentence + "</><br></center></div>";
-    } else {
-    	output = output + "<div id=\"vid" + videoID + "\">" +
-		"<br><b><center>Video " + videoID + "</b>     " +
-  		"(<a href='javaScript:requestVidDelete(\"" + videoID + "\")' style = 'filter: invert(22%) sepia(95%) saturate(7454%) hue-rotate(360deg) brightness(100%) contrast(117%)'><img src='trashcan.png' height=" + 14 + "></img></a>)" +
-   		"<br><video height=" + 150 + " controls>" + "<source src=\"" + url + "\" type=\"video/ogg\"></video>" +
-   		"<br><b>" + characterName + ": </b>" + sentence + "</><br></center></div>";
-    }
+    output = output + "<div id=\"vid" + videoID + "\">" +
+	"<br><b><center>Video " + videoID + "</b>     " +
+   	"<br><video height=" + 150 + " controls>" + "<source src=\"" + url + "\" type=\"video/ogg\"></video>" +
+   	"<br><b>" + characterName + ": </b>" + sentence + "</><br></center></div>";
   }
   
   vidList.innerHTML = output;
