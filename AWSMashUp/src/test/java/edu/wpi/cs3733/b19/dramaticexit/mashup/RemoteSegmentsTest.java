662819aa00cb8d6bb1be9914f5846d92286a81cd
ariannakan <kan.ari123@gmail.com>
2019-12-13 14:49:46 -0500

    finished remoteSegmentsTest - 75.6% coverage

--- AWSMashUp/src/test/java/edu/wpi/cs3733/b19/dramaticexit/mashup/RemoteSegmentsTest.java
+++ AWSMashUp/src/test/java/edu/wpi/cs3733/b19/dramaticexit/mashup/RemoteSegmentsTest.java
@@ -1,1 +1,34 @@
+package edu.wpi.cs3733.b19.dramaticexit.mashup;
 
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import edu.wpi.cs3733.b19.dramaticexit.mashup.http.RemoteSegmentsResponse;
+import edu.wpi.cs3733.b19.dramaticexit.mashup.model.Segment;
+
+/**
+ * A simple test harness for locally invoking your Lambda function handler.
+ */
+public class RemoteSegmentsTest extends LambdaTest {
+	
+    @Test
+    public void testGetList() throws IOException {
+    	RemoteSiteHandler handler = new RemoteSiteHandler();
+
+        RemoteSegmentsResponse resp = handler.handleRequest(null, createContext("list"));
+        
+        boolean hasTest0 = false;
+        for (Segment c : resp.segments) {
+        	System.out.println(c.character);
+        	if (c.character.equals("Trelane")) { hasTest0 = true; break; }
+        }
+        
+        System.out.println(hasTest0);
+        Assert.assertTrue(hasTest0);
+        Assert.assertEquals(resp.segments.size(), 4); //one video is not available
+        Assert.assertEquals(200, resp.statusCode);
+    }
+
+}
