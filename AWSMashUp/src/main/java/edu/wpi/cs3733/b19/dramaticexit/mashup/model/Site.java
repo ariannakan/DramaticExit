57a834057794f47146b9b2c06b3b5bfdf1c0b103
ariannakan <kan.ari123@gmail.com>
2019-12-13 15:16:36 -0500

    tested Site object

--- AWSMashUp/src/main/java/edu/wpi/cs3733/b19/dramaticexit/mashup/model/Site.java
+++ AWSMashUp/src/main/java/edu/wpi/cs3733/b19/dramaticexit/mashup/model/Site.java
@@ -6,10 +6,44 @@ public class Site {
 	public String url;
 	public boolean system;
 	
+	
+	public String getTimestampID() {
+		return timestampID;
+	}
+
+	public void setTimestampID(String timestampID) {
+		this.timestampID = timestampID;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
 	public Site(String siteID, String url) {
 		this.timestampID = siteID;
 		this.url = url;
 	}
 	
-
+	public Site() {
+		this.timestampID = "";
+		this.url = "";
+	}
+	
+	/**
+	 * Equality of Sites determined by name alone.
+	 */
+	public boolean equals (Object o) {
+		if (o == null) { return false; }
+		
+		if (o instanceof Site) {
+			Site other = (Site) o;
+			return url.equals(other.url);
+		}
+		
+		return false;  // not a Constant
+	}
 }
